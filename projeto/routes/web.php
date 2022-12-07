0586a822dc6bae6dc28e93e285638b5f59a105a9
Benjamim-EP <bannk18@gmail.com>
2022-12-07 13:18:54 -0300

    Rotas basico 1

--- projeto/routes/web.php
+++ projeto/routes/web.php
@@ -16,3 +16,40 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/inicio', function () {
+    echo 'ola';
+});
+
+// parametro opcional
+Route::get('/inicio/{nome?}', function ($nome1 = null) { 
+    if (isset($nome1)){  
+          echo "Olá ".$nome1."!";
+      }else{
+        echo "vc n digitou o nome";
+      }
+});
+
+// rota com regras
+Route::get('/rotacomregras/{nome}/{n}',function($nome,$n){
+    for($i=0;$i=$n;$i++){
+        echo "Ola! Seja bem vindo, $nome! <br>";
+    }
+})->where('nome','[A-Za-a]+')->where('n','[0-9]+');
+
+// Agrupamento de rotas
+Route::prefix('/app')->group(function(){
+
+    Route::get('/',function(){
+        return view('app');
+    });
+
+    
+    Route::get('/profile',function(){
+        return view('profile');
+    });
+
+    Route::get('/user',function(){
+        return view('user');
+    });
+});
