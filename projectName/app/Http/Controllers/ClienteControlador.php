2cc37f07605e8bc296ff062489d54cf49fb0efe6
Benjamim-EP <bannk18@gmail.com>
2022-12-08 14:43:24 -0300

    armazenando em sessão

--- projectName/app/Http/Controllers/ClienteControlador.php
+++ projectName/app/Http/Controllers/ClienteControlador.php
@@ -15,6 +15,12 @@ class ClienteControlador extends Controller
         
     ];
 
+    public function __construct(){
+        $clientes = session('clientes');
+        if(!isset($clientes))
+            session(['clientes' => $this->clientes]);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -22,7 +28,7 @@ class ClienteControlador extends Controller
      */
     public function index()
     {
-       $clientes = $this->clientes;
+       $clientes = session('clientes');
        return view('clientes.index',compact(['clientes']));
     }
 
@@ -44,14 +50,12 @@ class ClienteControlador extends Controller
      */
     public function store(Request $request)
     {
-        $id = count($this->clientes) + 1;
+        $clientes = session('clientes');
+        $id = count($clientes) + 1;
         $nome= $request->nome;
         $dados= ["id"=>$id, "nome"=>$nome];
-        $this->clientes[] = $dados;
-        //dd($this->clientes);
-        //return redirect()->route('clientes.index');
-        
-        $clientes = $this->clientes;
+        $clientes[] = $dados;
+        session(['clientes'=>$clientes]);
         return view('clientes.index', compact(['clientes']));
     }
 
