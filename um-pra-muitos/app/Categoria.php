d2d21bc704530ef33f030fe7e0c2489625eac0c6
Benjamim-EP <bannk18@gmail.com>
2022-12-26 15:32:33 -0300

    hasMany

--- um-pra-muitos/app/Categoria.php
+++ um-pra-muitos/app/Categoria.php
@@ -6,9 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    //
+    // 
     public function produtos() {
-        return $this->hasMany('App\Produto');
+        return $this->hasMany('App\Produto'); // quando se quer ir do "muitos" para "um"
     }
 
 }
+ 
