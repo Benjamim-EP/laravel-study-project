61c7aae40f53c346451208a1b7dbf9ec6fc7679d
Benjamim-EP <bannk18@gmail.com>
2022-12-26 11:38:02 -0300

    belongsTo

--- um-pra-muitos/routes/web.php
+++ um-pra-muitos/routes/web.php
@@ -14,3 +14,38 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/categorias', function () {
+    $cats = Categoria::all();
+    if (count($cats) === 0) {
+        echo "<h4>Você nao possui nenhuma categoria cadastrada</h4>";
+    }
+    else {
+        foreach($cats as $c) {
+            echo "<p>" . $c->id . ") " . $c->nome . "</p>";
+        }
+    }
+});
+
+Route::get('/produtos', function () {
+    $prods = Produto::all();
+    if (count($prods) === 0) {
+        echo "<h4>Você nao possui nenhum produto cadastrado</h4>";
+    }
+    else {
+        echo "<table>";
+        echo "<thead><tr><td>Nome</td><td>Estoque</td><td>Preco</td><td>Categoria</td></tr></thead>";
+        echo "<tbody>";
+        foreach($prods as $p) {
+            echo "<tr>";
+            echo "<td>" . $p->nome . "</td>";
+            echo "<td>" . $p->estoque . "</td>";
+            echo "<td>" . $p->preco . "</td>";
+            // echo "<td>" . $p->categoria_id . "</td>";
+            echo "<td>" . $p->categoria->nome . "</td>";
+            echo "</tr>";
+        }
+        echo "</tbody>";
+        echo "</table>";
+    }
+});
