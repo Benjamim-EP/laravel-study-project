d1ba66c903a512ef7dc7a8733872b8082b2535b5
Benjamim-EP <bannk18@gmail.com>
2023-01-25 21:15:33 -0300

    Login basico - rota

--- middlewares-login/routes/web.php
+++ middlewares-login/routes/web.php
@@ -11,8 +11,31 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/produtos','ProdutoControlador@index');
+
+Route::post('/login', function (Request $req) {
+    $login_ok = false;
+    switch($req->input('user')){
+        case 'joao':
+            $login_ok = $req->input('passwd') === 'senhajoao';
+            break;
+        case 'maria':
+            $login_ok = $req->input('passwd') === 'senhamaria';
+            break;
+        case 'jose':
+            $login_ok = $req->input('passwd') === 'senhajose';
+            break;
+    }
+    if($login_ok){
+        return response('Login OK', 200);
+    }
+    else{
+        return response('Falha no login', 404);
+    }
+});
