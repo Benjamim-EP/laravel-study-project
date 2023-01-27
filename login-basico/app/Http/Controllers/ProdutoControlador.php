5a450e724f99a76414febd3aed2a0696245b0c9f
Benjamim-EP <bannk18@gmail.com>
2023-01-27 15:58:19 -0300

    Criando e protegendo um controlador

---
+++ login-basico/app/Http/Controllers/ProdutoControlador.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutoControlador extends Controller
+{
+    public function __construct(){
+        //$this->middleware('auth');
+    }
+
+    public function index(){
+        echo "<h4>Lista de Produtos</h4>";
+        echo "<ul>";
+        echo "<li>Produto 1</li>";
+        echo "<li>Produto 2</li>";
+        echo "<li>Produto 3</li>";
+        echo "</ul>";
+    }
+}
--- login-basico/routes/web.php
+++ login-basico/routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/produtos', 'ProdutoControlador@index')
+    ->middleware('auth')
+    ->name('produtos'); 
