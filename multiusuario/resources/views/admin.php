68e906ae2a7fd45f68613b8f10a2e9a1402f28ac
Benjamim-EP <bannk18@gmail.com>
2023-01-30 13:30:35 -0300

    Criando controlador e views

--- multiusuario/resources/views/admin.php
+++ multiusuario/resources/views/admin.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Dashboard - Admin</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    Você está logado como usuário!
+                    Voce está logado como Admin!
                 </div>
             </div>
         </div>
