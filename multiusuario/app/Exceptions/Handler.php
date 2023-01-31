4a7b321c9dbf5141acb6ee9231cffc5772639550
Benjamim-EP <bannk18@gmail.com>
2023-01-31 11:28:25 -0300

    mais correções

--- multiusuario/app/Exceptions/Handler.php
+++ multiusuario/app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+////////// Adicionar ====>>>>>>>>>>>>>>
 use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
@@ -50,24 +52,27 @@ class Handler extends ExceptionHandler
         return parent::render($request, $exception);
     }
 
+
+    ////////// Adicionar ====>>>>>>>>>>>>>>
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
+        if ($request->expectsJson())
+            return response()->json(['message' => $exception->getMessage()], 401);
 
-        $guard = array_get($exception->guards(), 0);
-
+        $guard = array_get($exception->guards(),0);
         switch ($guard) {
             case 'admin':
                 $login = 'admin.login';
                 break;
-            
-            default:
+            case 'web':
                 $login = 'login';
                 break;
+            default:
+                $login = 'login';
         }
-
         return redirect()->guest(route($login));
     }
+
+
 }
+
--- multiusuario/app/Http/Middleware/RedirectIfAuthenticated.php
+++ multiusuario/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,11 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
+            if($guard == 'admin') 
+                return redirect()->route('admin.dashboard');
+            
             return redirect('/home');
+            
         }
 
         return $next($request);
